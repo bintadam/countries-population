d3357b01458e39b73c3874f4e13f75f9e29a44a0
bintadam <zakiyaadan12@gmail.com>
2023-07-21 22:37:01 +0200

    add useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const url = "https://restcountries.com/v2/all";
+    axios
+      .get(url)
+      .then((response) => setData(response.data))
+      .catch((error) => console.error(error))
+  }, [])
+
+  if(!data){
+    return <div>Loading...</div>
+  }
+
   return (
     <div className="App">
 
