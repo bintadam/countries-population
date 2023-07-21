b3abb232955c1c58b5888ea93632ad95f68535dd
bintadam <zakiyaadan12@gmail.com>
2023-07-21 22:51:43 +0200

    created function tenmostpopulated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-import axios from "axios"
+import axios from "axios";
 
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const url = "https://restcountries.com/v2/all";
@@ -17,9 +17,29 @@ function App() {
     return <div>Loading...</div>
   }
 
+  const populatedCountries = data.map((country) => {
+    return {
+      name: country.name,
+      population: country.population
+    }
+  })
+
+  const sortPopulatedCountries = populatedCountries.sort((a,b) => b.population - a.population)
+
+  const tenMostPopulated = sortPopulatedCountries.slice(0, 10);
+
+
   return (
     <div className="App">
+      <p className="world">World population</p>
+      <p className="populated">Ten most populated countries</p>
+      <div className='chart'>
+        {data.map((country, index) => (
+          <div key={index}>
 
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
