98437f766d6851bc67fa8faf2d3001066d4b4fe0
bintadam <zakiyaadan12@gmail.com>
2023-07-23 19:35:12 +0200

    add responsiveness

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   const isDesktop = useMediaQuery({query: "(min-width: 1224px)"});
-  const isMobile = useMediaQuery({query: "(min-width: 1224px)"});
+  const isMobile = useMediaQuery({query: "(max-width: 1224px)"});
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -40,16 +40,34 @@ function App() {
 
   return (
     <div className="App">
-        <h3 className="world">World population</h3>
-        <h6 className="populated">Ten most populated countries</h6>
-      <div className='chart'>
-        {tenMostPopulated.map((country, index) => (
-          <div key={index} className='bar' style={{width: `${country.population * scaleFactor}px`}}>
-              <p className="bar-name">{country.name}</p>
-              <p className="bar-label">{country.population.toLocaleString()}</p>
+      { isDesktop && (
+        <div>
+          <h3 className="world">World population</h3>
+          <h6 className="populated">Ten most populated countries</h6>
+          <div className='chart'>
+          {tenMostPopulated.map((country, index) => (
+            <div key={index} className='bar' style={{width: `${country.population * scaleFactor}px`}}>
+                <p className="bar-name">{country.name}</p>
+                <p className="bar-label">{country.population.toLocaleString()}</p>
+            </div>
+          ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
+      { isMobile && (
+        <div>
+          <h3 className="text-3xl text-center font-light m-4">World population</h3>
+          <h6 className="text-base text-center font-light mb-4">Ten most populated countries</h6>
+          <div className='bg-gray-300 p-3 w-screen'>
+          {tenMostPopulated.map((country, index) => (
+            <div key={index} className='bg-orange-400 ml-28 mt-2 h-10' style={{width: `${country.population * 200/maxPopulation }px`}}>
+                <p className="float-left text-xs font-light">{country.name}</p>
+                <p className="text-xs font-light ml-56">{country.population.toLocaleString()}</p>
+            </div>
+          ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
